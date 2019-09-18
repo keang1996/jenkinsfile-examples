3dad6613eeddf3cdaff04bb0223a112db1edd760
keang1996 <35160800+keang1996@users.noreply.github.com>
2019-09-18 15:30:10 +0700

    Update 050-shared-library-where-is-it-cloned.groovy

--- jenkinsfiles/050-shared-library-where-is-it-cloned.groovy
+++ jenkinsfiles/050-shared-library-where-is-it-cloned.groovy
@@ -3,7 +3,7 @@ library(
   retriever: modernSCM(
     [
       $class: 'GitSCMSource',
-      remote: 'https://github.com/hoto/jenkins-shared-library.git'
+      remote: 'https://github.com/keang1996/jenkins-shared-library.git'
     ]
   )
 )
